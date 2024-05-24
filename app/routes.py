8ae574d30ab176dc904eec0f39f2c7ca993bffbf
swejoycechoi <swejoycechoi@gmail.com>
2024-05-24 23:46:33 +0900

    templates added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'swejoycechoi'}
+    posts = [
+        {
+            'author': {'username': 'Hunter'},
+            'body': 'Machine learning in 2024'
+        },
+        {
+            'author': {'username': 'Pia'},
+            'body': 'Generative vs General AI'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
